487a02db38c09bd21ae34b24f7144b31e6ac9729
Tanishq Mishra <mishratanishq619@gmail.com>
2023-09-08 20:28:17 +0530

    first

---
+++ scripts.js
@@ -0,0 +1,19 @@
+let pos = document.getElementById("center");
+
+let body = document.querySelector("body");
+
+pos.addEventListener("mousemove", (e) => {
+	let recloc = pos.getBoundingClientRect();
+	console.log(parseInt(e.clientX - recloc.left));
+	let x = e.clientX - recloc.left;
+	console.log(x);
+	let clr = "";
+	if (x < 300) {
+		clr = `rgb(${255 - (x / 300) * 255}, 0, 0)`;
+	} else {
+		clr = `rgb(0, 0,${((x - 300) / 300) * 255})`;
+	}
+
+	pos.style.backgroundColor = clr;
+	body.style.backgroundColor = `rgb(0, 255,${((x - 300) / 300) * 255})`;
+});
