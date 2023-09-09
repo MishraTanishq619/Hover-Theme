bfd184adca64879e8aae8c401dcd99f7dc9fc15a
Tanishq Mishra <mishratanishq619@gmail.com>
2023-09-09 11:15:21 +0530

    used gsap mapRange

--- scripts.js
+++ scripts.js
@@ -4,21 +4,47 @@ let body = document.querySelector("body");
 
 pos.addEventListener("mousemove", (e) => {
 	let recloc = pos.getBoundingClientRect();
-	console.log(parseInt(e.clientX - recloc.left));
+	// console.log(parseInt(e.clientX - recloc.left));
 	let x = e.clientX - recloc.left;
-	console.log(x);
-	let clr = "";
-	if (x < 300) {
-		clr = `rgb(${255 - (x / 300) * 255}, 0, 0)`;
+	// console.log(x);
+
+	// console.log(gsap.utils.mapRange(0, recloc.width / 2, 255, 0, x));
+
+	// let clr = "";
+
+	if (x < recloc.width / 2) {
+		clr = `rgb(${gsap.utils.mapRange(
+			0,
+			recloc.width / 2,
+			255,
+			0,
+			x
+		)}, 0, 0)`;
 	} else {
-		clr = `rgb(0, 0,${((x - 300) / 300) * 255})`;
+		clr = `rgb(0, 0,${gsap.utils.mapRange(
+			recloc.width / 2,
+			recloc.width,
+			0,
+			255,
+			x
+		)})`;
 	}
 
-	pos.style.backgroundColor = clr;
+	// pos.style.backgroundColor = clr;
+
+	gsap.to(pos, {
+		backgroundColor: clr,
+	});
+
 	body.style.backgroundColor = `rgb(0, 255,${((x - 300) / 300) * 255})`;
 });
 
 pos.addEventListener("mouseleave", () => {
-	pos.style.backgroundColor = "white";
+	// pos.style.backgroundColor = "white";
+
+	gsap.to(pos, {
+		backgroundColor: "white",
+	});
+
 	body.style.backgroundColor = "white";
 });
